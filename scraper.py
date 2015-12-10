2eedf08a1102fe9314295badeaa7ff9054c9a2f3
blablupcom <olegvolodin@eyahoo.com>
2015-12-10 08:47:28 +0300

    first commit

--- scraper.py
+++ scraper.py
@@ -1,104 +1,161 @@
 # -*- coding: utf-8 -*-
+
+#### IMPORTS 1.0
+
 import os
 import re
-import requests
 import scraperwiki
 import urllib2
 from datetime import datetime
 from bs4 import BeautifulSoup
-from dateutil.parser import parse
-from dateutil.rrule import rrule, MONTHLY
-from datetime import time
-import time
-from datetime import datetime
-from datetime import datetime
 
-# Set up variables
-entity_id = "E2321_LCC_gov"
-st_date = datetime(int(2014),int(1),int(1))
-end_date = datetime(int(2015),int(6),int(1))
-start_urls = ['http://www3.lancashire.gov.uk/transparency/spending/SupplierPayments.asp?Period=%s&Version=1' % dt.strftime('%m%Y') for dt in rrule(MONTHLY, dtstart=st_date, until=end_date)]
-data = {'strAction': 'exportCSV', 'strOutputFields':'service_lable', 'strOutputFields':'service_code',
-        'strOutputFields':'organisational_unit', 'strOutputFields':'expenditure_category',
-        'strOutputFields':'expenditure_code', 'strOutputFields': 'payment_date', 'strOutputFields':'transaction_number',
-        'strOutputFields':'amount', 'strOutputFields':'supplier_name'}
+#### FUNCTIONS 1.2
 
+import requests    #  import requests to validate url
 
-#url = 'http://www3.lancashire.gov.uk/transparency/spending/SupplierPayments.asp?Period=062015&Version=1'
-errors = 0
-# Set up functions
 def validateFilename(filename):
-    filenameregex = '^[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[0-9][0-9][0-9][0-9]_[0-9][0-9]$'
-    dateregex = '[0-9][0-9][0-9][0-9]_[0-9][0-9]'
+    filenameregex = '^[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[0-9][0-9][0-9][0-9]_[0-9QY][0-9]$'
+    dateregex = '[0-9][0-9][0-9][0-9]_[0-9QY][0-9]'
     validName = (re.search(filenameregex, filename) != None)
     found = re.search(dateregex, filename)
     if not found:
         return False
     date = found.group(0)
-    year, month = int(date[:4]), int(date[5:7])
     now = datetime.now()
-    validYear = (2000 <= year <= now.year)
-    validMonth = (1 <= month <= 12)
+    year, month = date[:4], date[5:7]
+    validYear = (2000 <= int(year) <= now.year)
+    if 'Q' in date:
+        validMonth = (month in ['Q0', 'Q1', 'Q2', 'Q3', 'Q4'])
+    elif 'Y' in date:
+        validMonth = (month in ['Y1'])
+    else:
+        try:
+            validMonth = datetime.strptime(date, "%Y_%m") < now
+        except:
+            return False
     if all([validName, validYear, validMonth]):
         return True
-def validateURL(url):
+
+
+def validateURL(url, requestdata):
     try:
-        r = requests.post(url, data =data, allow_redirects=True, timeout=20)
+        r = requests.post(url, data = requestdata, allow_redirects=True, timeout=20)
         count = 1
         while r.status_code == 500 and count < 4:
             print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
             count += 1
-            r = requests.post(url, data =data, allow_redirects=True, timeout=20)
+            r = requests.post(url, data = requestdata, allow_redirects=True, timeout=20)
         sourceFilename = r.headers.get('Content-Disposition')
+
         if sourceFilename:
             ext = os.path.splitext(sourceFilename)[1].replace('"', '').replace(';', '').replace(' ', '')
-        elif r.headers['Content-Type'] == 'text/csv':
-             ext = '.csv'
         else:
             ext = os.path.splitext(url)[1]
         validURL = r.status_code == 200
-        validFiletype = ext in ['.csv', '.xls', '.xlsx', '.CSV']
+        validFiletype = ext.lower() in ['.csv', '.xls', '.xlsx']
         return validURL, validFiletype
     except:
-        raise
-def convert_mth_strings ( mth_string ):
-
-    month_numbers = {'JAN': '01', 'FEB': '02', 'MAR':'03', 'APR':'04', 'MAY':'05', 'JUN':'06', 'JUL':'07', 'AUG':'08', 'SEP':'09','OCT':'10','NOV':'11','DEC':'12' }
-    #loop through the months in our dictionary
-    for k, v in month_numbers.items():
-#then replace the word with the number
-
-        mth_string = mth_string.replace(k, v)
-    return mth_string
-# pull down the content from the webpage
+        print ("Error validating URL.")
+        return False, False
 
 
-for url in start_urls:
-    html = requests.post(url, data =data,  allow_redirects=True)
-    csvYr = url.split('&')[0].split('=')[-1][-4:]
-    csvMth = url.split('&')[0].split('=')[-1][:2]
-    filename = entity_id + "_" + csvYr + "_" + csvMth
-    todays_date = str(datetime.now())
-    file_url = url
+def validate(filename, file_url, requestdata):
     validFilename = validateFilename(filename)
-    validURL, validFiletype = validateURL(file_url)
+    validURL, validFiletype = validateURL(file_url, requestdata)
     if not validFilename:
         print filename, "*Error: Invalid filename*"
         print file_url
-        errors += 1
-        continue
+        return False
     if not validURL:
         print filename, "*Error: Invalid URL*"
         print file_url
-        errors += 1
-        continue
+        return False
     if not validFiletype:
         print filename, "*Error: Invalid filetype*"
         print file_url
+        return False
+    return True
+
+def convert_mth_strings ( mth_string ):
+    month_numbers = {'JAN': '01', 'FEB': '02', 'MAR':'03', 'APR':'04', 'MAY':'05', 'JUN':'06', 'JUL':'07', 'AUG':'08', 'SEP':'09','OCT':'10','NOV':'11','DEC':'12' }
+    for k, v in month_numbers.items():
+        mth_string = mth_string.replace(k, v)
+    return mth_string
+
+#### VARIABLES 1.0
+
+entity_id = "E2321_LCC_gov"
+url = 'http://www3.lancashire.gov.uk/transparency/spending/SupplierPayments.asp?sysredir=y'
+requestdata = {'strAction': 'exportCSV', 'strOutputFields':'service_lable', 'strOutputFields':'service_code',
+        'strOutputFields':'organisational_unit', 'strOutputFields':'expenditure_category',
+        'strOutputFields':'expenditure_code', 'strOutputFields': 'payment_date', 'strOutputFields':'transaction_number',
+        'strOutputFields':'amount', 'strOutputFields':'supplier_name'}
+errors = 0
+data = []
+
+#### READ HTML 1.0
+
+html = urllib2.urlopen(url)
+soup = BeautifulSoup(html, 'lxml')
+
+
+#### SCRAPE DATA
+
+links = soup.find('ul', 'pal_submenu').find_all('a')
+for link in links:
+    url = 'http://www3.lancashire.gov.uk'+link['href']
+    csvYr = link['href'].split('&')[0].split('=')[-1][-4:]
+    csvMth = link['href'].split('&')[0].split('=')[-1][:2]
+    data.append([csvYr, csvMth, url, requestdata])
+
+
+#### STORE DATA 1.0
+
+for row in data:
+    csvYr, csvMth, url, requestdata = row
+    filename = entity_id + "_" + csvYr + "_" + csvMth
+    todays_date = str(datetime.now())
+    file_url = url.strip()
+
+    valid = validate(filename, file_url, requestdata)
+
+    if valid == True:
+        scraperwiki.sqlite.save(unique_keys=['l'], data={"l": file_url, "f": filename, "d": todays_date })
+        print filename
+    else:
         errors += 1
-        continue
-    scraperwiki.sqlite.save(unique_keys=['l'], data={"l": file_url, "f": filename, "d": todays_date })
-    print filename
 
 if errors > 0:
-     raise Exception("%d errors occurred during scrape." % errors)
+    raise Exception("%d errors occurred during scrape." % errors)
+
+
+#### EOF
+
+
+#### EOF
+
+#     filename = entity_id + "_" + csvYr + "_" + csvMth
+#     todays_date = str(datetime.now())
+#     file_url = url
+#     validFilename = validateFilename(filename)
+#     validURL, validFiletype = validateURL(file_url)
+#     if not validFilename:
+#         print filename, "*Error: Invalid filename*"
+#         print file_url
+#         errors += 1
+#         continue
+#     if not validURL:
+#         print filename, "*Error: Invalid URL*"
+#         print file_url
+#         errors += 1
+#         continue
+#     if not validFiletype:
+#         print filename, "*Error: Invalid filetype*"
+#         print file_url
+#         errors += 1
+#         continue
+#     scraperwiki.sqlite.save(unique_keys=['l'], data={"l": file_url, "f": filename, "d": todays_date })
+#     print filename
+#
+# if errors > 0:
+#      raise Exception("%d errors occurred during scrape." % errors)
